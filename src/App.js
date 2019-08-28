5037937e85f1da60867561130a9692099c459423
Menana <anismenana@gmail.com>
2019-08-28 12:33:22 +0100

    code cleanUp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
-
 import ParticleComponent from "./ParticleComponent";
 import { BrowserRouter as Router, Link, NavLink } from "react-router-dom";
 import Route from "react-router-dom/Route";
